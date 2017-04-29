4590747473501b2868b5f560cefdf7aa972470f5
maximilianhuber <maximilian.huber@tngtech.com>
2017-04-29 15:09:37 +0200

    add favicon

--- site.hs
+++ site.hs
@@ -1,51 +1,55 @@
-  --------------------------------------------------------------------------------
-  {-# LANGUAGE OverloadedStrings #-}
-  import           Data.Monoid (mappend)
-  import           Hakyll
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
 
 
-  --------------------------------------------------------------------------------
-  main :: IO ()
-  main = hakyll $ do
-      match "materialize/**/*" $ do
-          route idRoute
-          compile copyFileCompiler
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "favicon.png" $ do
+        route idRoute
+        compile copyFileCompiler
 
-      match "images/*" $ do
-          route   idRoute
-          compile copyFileCompiler
+    match "materialize/**/*" $ do
+        route idRoute
+        compile copyFileCompiler
 
-      match "js/*" $ do
-              route   idRoute
-              compile copyFileCompiler
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
 
-      match "fonts/**/*" $ do
-              route   idRoute
-              compile copyFileCompiler
+    match "js/*" $ do
+            route   idRoute
+            compile copyFileCompiler
 
-      match "css/*" $ do
-          route   idRoute
-          compile compressCssCompiler
+    match "fonts/**/*" $ do
+            route   idRoute
+            compile copyFileCompiler
 
-      match (fromList ["impressum.md"]) $ do
-          route   $ setExtension "html"
-          compile $ pandocCompiler
-              >>= loadAndApplyTemplate "templates/container.html" defaultContext
-              >>= loadAndApplyTemplate "templates/default.html" defaultContext
-              >>= relativizeUrls
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
 
-      match "*.html" $ do
-          route idRoute
-          compile $ do
-              getResourceBody
-                  >>= loadAndApplyTemplate "templates/default.html" defaultContext
-                  >>= relativizeUrls
+    match (fromList ["impressum.md"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/container.html" defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
 
-      match "templates/*" $ compile templateBodyCompiler
+    match "*.html" $ do
+        route idRoute
+        compile $ do
+            getResourceBody
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateBodyCompiler
 
 
-  --------------------------------------------------------------------------------
-  postCtx :: Context String
-  postCtx =
-      dateField "date" "%B %e, %Y" `mappend`
-      defaultContext
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
